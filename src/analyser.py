9cdfc288b56379483cbef7af854dedebfc10b849
Smetanin Alexander <riserisen@yandex.ru>
2019-10-11 19:51:22 +0300

    frequency calculator

--- src/analyser.py
+++ src/analyser.py
@@ -1,23 +1,114 @@
-from src.populations import Population
-from math import log
+import pathlib
+import vcf
+import subprocess
+
 from queue import PriorityQueue
 from operator import itemgetter
+from math import log
+
+from src.populations import Population
+
+
+BASE_PATH = pathlib.Path(__file__).parents[1]
+
+
+class FrequencyCalculator:
+
+    def __init__(self, chromosome, start, end, populations=None):
+        self.populations = populations or Population().pair
+        self._configure()
+        self._set_segment(chromosome, start, end)
+
+    def _set_segment(self, chromosome, start, end):
+        """Set chromosome and segment"""
+        self.chromosome = chromosome
+        self.start = start
+        self.end = end
+
+    def _configure(self, local_dir=None, file_pattern=None, base_url=None):
+        """Configure paths and patterns"""
+        self.base_url = base_url or 'ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/'
+        self.pattern = file_pattern or 'ALL.chr{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
+        self.local_dir = local_dir or BASE_PATH / 'data'
+
+    def _path(self, file, absolute=True):
+        return str(self.local_dir / file) if absolute else file
+
+    def _samples_path(self, pop):
+        """Path to samples from population"""
+        return self._path(f'{pop}.samples.list')
+
+    def _segment_path(self):
+        """Path to current """
+        return self._path(f'chr{self.chromosome}.{self.start}_{self.end}.vcf.gz')
+
+    def _pop_segment_path(self, pop):
+        return self._path(f'{pop}.chr{self.chromosome}.{self.start}-{self.end}.vcf.gz')
+
+    def _check_local(self):
+        # TODO: implement this
+        return False
+
+    def fetch(self):
+        if self._check_local():
+            return
+        file = f'{self.base_url}{self.pattern.format(self.chromosome)}'
+        query = f'{self.chromosome}:{self.start}-{self.end}'
+        local_filename = self._segment_path()
+
+        load_command = ' '.join(['tabix', '-h', file, query, '|', 'bgzip', '-c', '>', local_filename])
+        subprocess.call(load_command, shell=True, stdout=subprocess.PIPE)
+
+        index_command = ' '.join(['tabix', '-p', 'vcf', local_filename])
+        subprocess.call(index_command, shell=True, stdout=subprocess.PIPE)
+
+    def index(self, filename):
+        pass
+
+    def samples(self, pop):
+        command = ' '.join(['grep', pop, self._path('samples.tsv'), '|', 'cut', '-f1', '>', self._samples_path(pop)])
+        subprocess.call(command, shell=True, stdout=subprocess.PIPE)
+
+    def subset(self, pop):
+        local_filename = self._segment_path()
+        command = ' '.join(['vcf-subset', '-c', self._samples_path(pop), local_filename, '|', 'fill-an-ac',
+                           '|', 'bgzip', '-c', '>',  self._pop_segment_path(pop)])
+        subprocess.call(command, shell=True)
+
+    def calculate_frequency(self, pop):
+        reader = vcf.Reader(filename=self._pop_segment_path(pop))
+        dict = {}
+        for record in reader:
+            allele_num = record.INFO['AN']
+            allele_counts = [allele_num - sum(record.INFO['AC'])] + record.INFO['AC']
+            dict[record.POS] = [ac / allele_num for ac in allele_counts]
+        return dict
+
+    def execute(self):
+        self.fetch()
+        population_frequencies = {}
+        for pop in self.populations:
+            self.samples(pop)
+            self.subset(pop)
+            population_frequencies[pop] = self.calculate_frequency(pop)
+        print(population_frequencies)
 
 
 def i4a(record, populations=None):
     """Calculate informativeness for assignment"""
     alleles = [record.REF] + record.ALT
-    populations = populations or Population.all
+    populations = populations or Population.superpopulations
     s = 0
     N = len(alleles)
     K = len(populations)
-    # population_allele_frequencies
+    # population allele frequencies
     paf = {
         pop: [1 - sum(record.INFO[pop + '_AF'])] + record.INFO[pop + '_AF'] for pop in populations
     }
-    mean_frequencies = [sum(paf[pop][i] for pop in populations) / K for i in range(N)]
+    # average allele frequencies
+    aaf = [sum(paf[pop][i] for pop in populations) / K for i in range(N)]
     for j in range(N):
-        p = mean_frequencies[j]
+        p = aaf[j]
         s -= p * log(p) if p != 0 else 0
         for pop in populations:
             q = paf[pop][j]
@@ -36,4 +127,5 @@ def get_best_markers(records, populations, capacity=10):
             last = q.get()
             q.put(max((informativeness, record), last, key=itemgetter(0)))
     return [q.get() for _ in range(q.qsize())]
+
 
--- src/loader.py
+++ src/loader.py
@@ -1,13 +1,16 @@
 """
 Basic module for loading 1000 genomes data
 """
-from contextlib import contextmanager, closing
 import shutil
+import pathlib
+import subprocess
 import urllib.request as request
 
+from contextlib import contextmanager, closing
 from ftplib import FTP
 
-from src.parser import BASE_PATH
+
+BASE_PATH = pathlib.Path(__file__).parents[1]
 
 
 class Loader:
@@ -15,8 +18,15 @@ class Loader:
     def __init__(self, base_url=None, base_path=None):
         self.base_url = base_url or 'ftp.1000genomes.ebi.ac.uk/vol1/ftp/'
         self.base_path = base_path or BASE_PATH
-        self.login = None
-        self.password = None
+
+        self._set_credentials()
+        self._configure()
+
+    def _configure(self,  collection_path=None, file_pattern=None):
+        self.collection_path = self.base_url + (collection_path or 'release/20130502/')
+
+        default_pattern = 'ALL.chr{chrom}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
+        self.file_pattern = file_pattern or default_pattern
 
     def local_path(self, filepath):
         filename = filepath.split('/')[-1]
@@ -27,7 +37,7 @@ class Loader:
             with open(self.local_path(filepath), 'wb') as f:
                 shutil.copyfileobj(remote, f)
 
-    def set_credentials(self, login, password):
+    def _set_credentials(self, login=None, password=None):
         self.login = login
         self.password = password
 
@@ -41,4 +51,16 @@ class Loader:
     def ftp_load(self, filepath):
         with self.ftp_keeper(self.base_url) as ftp, open(self.local_path(filepath), 'wb') as f:
             ftp.retrbinary('RETR ' + filepath, f.write)
+
+    def slice(self, chrom, start, end):
+        file = f'ftp://{self.collection_path}{self.file_pattern.format(chrom=chrom)}'
+        query = f'{chrom}:{start}-{end}'
+        local_filename = f'chr{chrom}.{start}_{end}.vcf.gz'
+
+        load_command = ' '.join(['tabix', '-h', file, query, '|', 'bgzip', '-c', '>', local_filename])
+        subprocess.call(load_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        index_command = ' '.join(['tabix', '-p', 'vcf', local_filename])
+        subprocess.call(index_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
 
