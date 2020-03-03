5ca2e51958d60e67a6f6cc22cd144c4dc7957e2d
Smetanin Alexander <aleksandr_smetanin@epam.com>
2020-03-03 11:25:28 +0300

    add logging and fix bugs

--- src/loader.py
+++ src/loader.py
@@ -1,19 +1,26 @@
 """
 Basic module for loading 1000 genomes data
 """
+import sys
 import ftplib
 import shutil
 import pathlib
+import logging
 import subprocess
 import urllib.request as request
 
 from contextlib import closing
 
 
+logger = logging.getLogger(__name__)
+logger.setLevel('INFO')
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
 BASE_PATH = pathlib.Path(__file__).parents[1]
 
 
-# TODO: add logging
+# TODO: enhance logging
+# TODO: add loading from custom ftp
 class Loader:
     def __init__(self, chromosome, start, end, out_dir):
         self.start = start
@@ -25,9 +32,10 @@ class Loader:
         self.chr_file = f'ALL.chr{chromosome}.shapeit2_integrated_v1a.GRCh38.20181129.phased.vcf.gz'
 
     def _ensure_dir(self, out_dir):
-        self.out_dir = out_dir or f'chr{self.chromosome}_data'
+        self.out_dir = BASE_PATH / (out_dir or f'chromosome{self.chromosome}_data')
+        logger.info(f'Chromosome data will be stored in {self.out_dir}')
 
-        path = pathlib.Path(BASE_PATH / self.out_dir)
+        path = pathlib.Path(self.out_dir)
         if not path.exists():
             path.mkdir(parents=True, exist_ok=True)
 
@@ -51,7 +59,6 @@ class Loader:
 
     @property
     def local_filepath(self):
-
         return str(BASE_PATH / self.out_dir / self.local_filename)
 
     def _fetch(self):
@@ -59,17 +66,22 @@ class Loader:
         ftp.login()
 
         ftp.cwd(self.collection)
-        ftp.retrlines('LIST')
 
+        logger.info('Loading the chromosome, this may take some time.')
         ftp.retrbinary("RETR {}".format(self.chr_file), open(self.local_filepath, 'wb').write)
+
+        logger.info('Loading the index, this should be fast.')
+        ftp.retrbinary("RETR {}.tbi".format(self.chr_file), open(self.local_filepath + '.tbi', 'wb').write)
         ftp.quit()
 
     def _slice(self):
-        load_command = ' '.join([f'mkdir {self.out_dir} && cd {self.out_dir} &&', 'tabix', '-h',
+        load_command = ' '.join([f'cd {self.out_dir} &&', 'tabix', '-h',
                                  self.ftp_path, self.query, '|', 'bgzip', '-c', '>', self.local_filename])
+        logger.debug(f'Running command:\n{load_command}')
         subprocess.call(load_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
         index_command = ' '.join([f'cd {self.out_dir} &&', 'tabix', '-p', 'vcf', self.local_filename])
+        logger.debug(f'Running command:\n{index_command}')
         subprocess.call(index_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     def _load(self):
@@ -79,8 +91,10 @@ class Loader:
 
     def fetch(self):
         if self.slice_mode:
+            logger.info('Running in slice mode. Will only download requested region.')
             self._slice()
         else:
+            logger.info('Running in fetch mode. Will download full chromosome.')
             self._fetch()
 
 
